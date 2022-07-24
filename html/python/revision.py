f76a3b3fb210d5c19f5968093372bc5abe0a654a
clementineir <109534289+clementineir@users.noreply.github.com>
2022-07-24 23:22:48 +0200

    Add files via upload

---
+++ html/python/revision.py
@@ -0,0 +1,121 @@
+# print("Iribagiza clementine")
+
+# got="game of drones"
+# print(got)
+# print("$")
+# empty=""
+# print(empty)
+# print(len(got))
+# print(got[0])
+
+
+# name = input("Enter Employee Name ")
+# salary = input("Enter salary ")
+# company = input("Enter Company name ")
+# print("\n")
+# print("Printing Employee Details")
+# print("Name", "Salary", "Company")
+# print(name, salary, company)
+ 
+# light="blue"
+# if light=="green":
+#     print("go")
+# elif light=="yellow":
+#     print("caution")
+# elif light=="red":
+#     print("stop")
+# else:
+#     print("incorrect light signal")     
+
+#dictionary
+
+# ages={
+#     "peter":10,
+#     "agnes":12,
+#      "john":11,
+#     "yve":22,
+#      "vava":30,
+#     "rebeca":12,
+#      "eria":40,
+#     "karina":32,
+# }
+# print("the age of vava")
+# print(ages["vava"])
+
+# print("all ages")
+# for key,value in ages.items():
+#     print(key,value)
+
+
+# n=50
+# num_list=[10,4,23,6,18,27,47]
+# for n1 in num_list:
+#     for n2 in num_list:
+#         if n1+n2==n:
+#           print(n1,n2)
+
+# sum=0
+# for i in range(0,11):
+#     sum=sum+i
+# print(sum)
+
+
+
+# i=0
+# sum=0
+# while(i <=10):
+#     sum=sum+i
+# print(sum)
+
+
+# string_list=["python",2,"language"]
+# string_list1=["inana",4,"umuana"]
+# for i in string_list:
+#     if i== "language":
+#       print(i)
+# print(string_list+string_list1)
+# string_list.append("LLB")
+# print(string_list)
+# string_list.insert(0,"better")
+# print(string_list)
+
+# string_list.count("language")
+# print(string_list)
+# string_list.index("language")
+# print(string_list.index("language"))
+# string_list.sort()
+# print(string_list)
+
+information={
+   "name" :"john","age":23
+}
+print(information["name"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
